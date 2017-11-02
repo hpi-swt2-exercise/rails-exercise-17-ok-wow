c29ebc004747eb4af61de32ceddb0ef4dfd8804e
Conrad Lempert <conradlempert@Conrads-MBP.fritz.box>
2017-11-02 02:49:22 +0100

    added new_author_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'authors/new'
+  get 'authors/new' => 'pages/authors/new', :as => :new_author
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "New author page", type: :feature do
 
   it "should render withour error" do
-    visit 'authors/new'
+    visit new_author_path
   end
 end
