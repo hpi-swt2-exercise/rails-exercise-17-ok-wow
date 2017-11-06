5c656ef9c3706f4109e6a3cb860885ce193bd676
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-06 18:01:21 +0100

    siudbfapuidfpai

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -6,6 +6,7 @@ describe "author model test", type: :model do
     #author = Author.new(['Alan','Turing','http://wikipedia.org/Alan_Turing']);
     @author = FactoryGirl.create :author
     expect(@author.first_name).to eq('Alan');
+    expect(@author.name).to eq('Alan Turing');
   end
 
 
