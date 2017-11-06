316370add6be28fb39754d066a4ee7be4dac0fac
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-06 17:49:15 +0100

    fixed test 88

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :author do
-    firstname "MyString"
-    lastname "MyString"
-    homepage "MyString"
+    first_name "Alan"
+    last_name "Turing"
+    homepage "http://wikipedia.org/Alan_Turing"
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe "author model test", type: :model do
 
   it "should render withour error" do
-    author = Author.new('Alan','Turing','http://wikipedia.org/Alan_Turing');
-    expect(author.first_name).to eq('Alan');
+    #author = Author.new(['Alan','Turing','http://wikipedia.org/Alan_Turing']);
+    @author = FactoryGirl.create :author
+    expect(@author.first_name).to eq('Alan');
   end
 
 
