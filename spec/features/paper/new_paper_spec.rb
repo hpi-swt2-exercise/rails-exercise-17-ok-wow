0e4acc9090e91eb9ccc77febf14796c59d1622b7
Conrad Lempert <>
2017-11-07 12:36:21 +0100

    fixed issue 19

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,6 +5,10 @@ describe 'When i visit new paper page' do
   it 'should render without error' do
 
     visit new_paper_path
+    expect(page).to have_field('Title')
+    expect(page).to have_field('Venue')
+    expect(page).to have_field('Year')
+    expect(page).to have_css('input[type="submit"]')
 
   end
 end
