4433c2c9d8f7bf7d4c63553599a72f2e6e62c4ad
Conrad Lempert <>
2017-11-07 09:31:06 +0100

    fixed issue 8

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe 'When i visit authors index page' do
 
-  it 'should render without error' do
-
+  it 'should list name and homepage of all authors' do
+    @author = FactoryGirl.create :author
     visit "/authors"
+    expect(page).to have_text('Alan Turing')
+    expect(page).to have_text("http://wikipedia.org/Alan_Turing")
   end
 end
