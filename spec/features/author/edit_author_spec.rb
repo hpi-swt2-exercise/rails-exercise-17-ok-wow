3e9d94bb0003a0212bfe6fd3398f8ccd4d88f37a
Conrad Lempert <>
2017-11-07 10:52:00 +0100

    fixed issue 15

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -5,5 +5,7 @@ describe 'When a user visits the edit author page' do
   it 'should render without error' do
     @author = FactoryGirl.create :author
     visit "authors/#{@author.id}/edit"
+    fill_in 'author_first_name', :with => 'Alan Mathison'
+    click_button 'Save Author'
   end
 end
